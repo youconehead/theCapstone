dd61cc48e61a518df0d24a9b0785579e9bee3699
youconehead <artinhuule@gmail.com>
2026-09-15 20:12:02 -0700

    MAGIC 8 BALL

---
+++ fortuneteller/script.js
@@ -0,0 +1,43 @@
+let fortunes = ["It is certain","Without a doubt", "Yes - defintely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"];
+
+// Hash function
+function cyrb128(str) {
+  let h1 = 1779033703,
+    h2 = 3144134277,
+    h3 = 1013904242,
+    h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  (h1 ^= h2 ^ h3 ^ h4), (h2 ^= h1), (h3 ^= h1), (h4 ^= h1);
+  return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
+// Seeded pseudo-random number generator
+function mulberry32(a) {
+  return function () {
+    var t = (a += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
+}
+
+window.onload = function () {
+  var queryString = new URLSearchParams(window.location.search).toString();
+  if (queryString.length != 0) {
+    var hash = cyrb128(queryString);
+    let prng = mulberry32(hash[0]);
+    let rand = prng();
+    let randomIndex = Math.floor(rand * fortunes.length);
+    document.getElementById("fortune").textContent = "Fortune: " + fortunes[randomIndex];
+  }
+};
